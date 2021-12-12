bb468fb5c287cc62cccf5bb1517e8904804402b7
alihasanshaikh <alihasanshaikh2991@gmail.com>
2021-12-12 21:37:03 +0530

    master

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import './Checkout.css';
+import React from "react";
+import "./Checkout.css";
 import Subtotal from "./Subtotal";
 
-
 function Checkout() {
-    return (
-        <div className="checkout">
-            <div className="checkout__left">
-                <img className="checkout__adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
-                <div>
-                    <h3 className="checkout__title">Your Shopping Details</h3>
-                </div>
-
-            </div>
-
-            <div className="checkout__right">
-               <Subtotal />
-                    
-               
-            </div>
-            
+  return (
+    <div className="checkout">
+      <div className="checkout--left">
+          <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
+        <div>
+          <h3 className="checkout--title">Your Shopping Details</h3>
+          {/* Cart Item here */}
+          {/* Cart Item here */}
         </div>
-    );
+      </div>
+
+      <div className="checkout--right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
